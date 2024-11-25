fe1a0af3da91d0be9955dc8161cd4611584dd992
Your Name <your-email@example.com>
2024-11-24 17:17:11 -0800

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,5 @@
 import "./globals.css"
-import UserProvider from './context/user'
-import CartProvider from './context/cart'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 export const metadata = {
   title: 'eBay Clone',
@@ -14,15 +11,7 @@ export default function RootLayout({ children }) {
  return (
     <html lang="en">
       <body>
-        <div>
-          <ToastContainer />
-
-          <UserProvider>
-            <CartProvider>
-              {children}
-            </CartProvider>
-          </UserProvider>
-        </div>
+       {children}
       </body>
     </html>
   )
